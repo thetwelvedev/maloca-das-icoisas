98a9ced5867f1e56f0ab1ffe59ace3ebbda34444
Leonardo Castro <twelve.programming@gmail.com>
2024-09-20 15:14:33 -0400

    Primeiros passos com o tkinter

---
+++ Módulo 3/MIC 010/interface.py
@@ -0,0 +1,4 @@
+from tkinter import * # carrega todos os módulos da biblioteca
+
+janela = Tk() # Inicializa uma janela vazia
+janela.mainloop() # Cria um sistema de loop e fica a espera de algum evento.
