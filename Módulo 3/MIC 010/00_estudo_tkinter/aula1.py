de0d34cf6393e30c02a364e00445cb9cbb728695
Leonardo Castro <twelve.programming@gmail.com>
2024-09-20 23:30:14 -0400

    Primeiros passos com o tkinter

---
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula1.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+janela = Tk() #Cria a janela
+janela.title("Olá Mundo") #Título que fica no cabeçalho da janela
+janela.geometry('1000x500') #Tamanho da janela largura x comprimento
+janela.mainloop() #Faz ficar aberto a janela
