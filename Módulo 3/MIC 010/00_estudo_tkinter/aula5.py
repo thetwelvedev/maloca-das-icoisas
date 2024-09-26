b28369a76d290560b1243e93cc9443af0b15f93d
Leonardo Castro <twelve.programming@gmail.com>
2024-09-26 17:05:24 -0400

    Aprendendo o uso da função get

--- Módulo 3/MIC 010/00_estudo_tkinter/aula5.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula5.py
@@ -5,9 +5,29 @@ janela = Tk()
 janela.title('Entry')
 janela.geometry("300x300")
 
-label = Label(janela, text="Entry") 
-label.grid(row=0, column=0) 
-entrada = Entry(janela, width=10) #adiciona o entrada de dados
-entrada.grid(row=0, column=1) 
+label_nome = Label(janela, text="Nome: ") 
+label_nome.grid(row=0, column=0) 
+nome = Entry(janela, width=20) #adiciona o entrada de dados
+nome.grid(row=0, column=1) 
+
+label_idade = Label(janela, text="Idade: ") 
+label_idade.grid(row=1, column=0) 
+idade = Entry(janela, width=20) #adiciona o entrada de dados
+idade.grid(row=1, column=1) 
+
+label_pais = Label(janela, text="País: ") 
+label_pais.grid(row=2, column=0) 
+pais = Entry(janela, width=20) #adiciona o entrada de dados
+pais.grid(row=2, column=1) 
+
+def ler_entrada():
+    n = nome.get() #get serve para ler os dados da Entry
+    i = idade.get()
+    p = pais.get()
+
+    print(n, i, p)
+
+btn = Button(janela, text="Clique", bg="blue", command=ler_entrada)
+btn.grid(row=3, column=0)
 
 janela.mainloop()
