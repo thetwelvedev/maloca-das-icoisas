5128a7f6a61cead3692ac84b535ac5aab40a4ba3
Leonardo Castro <twelve.programming@gmail.com>
2024-09-26 17:22:54 -0400

    Apredendo a usar o .config

--- Módulo 3/MIC 010/00_estudo_tkinter/aula5.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula5.py
@@ -11,8 +11,8 @@ nome = Entry(janela, width=20) #adiciona o entrada de dados
 nome.grid(row=0, column=1) 
 
 label_idade = Label(janela, text="Idade: ") 
-label_idade.grid(row=1, column=0) 
-idade = Entry(janela, width=20, state="disable") #state="disable" ela deixa inativa  a entry
+label_idade.grid(row=1, column=0)
+idade = Entry(janela, width=20) #state="disable" ela deixa inativa  a entry
 idade.grid(row=1, column=1) 
 
 label_pais = Label(janela, text="País: ") 
@@ -25,9 +25,14 @@ def ler_entrada():
     i = idade.get()
     p = pais.get()
     label_saida = Label(janela, text=f"{n}, {i}, {p}")
-    label_saida.grid(row=4, column=0)
+    label_saida.grid(row=5, column=0)
 
+def desativar_entry():
+    idade.config(state="disabled")  # .config() serve para alterar ou configurar as propriedades de um widget já existente
+    
 btn = Button(janela, text="Clique", bg="blue", command=ler_entrada)
 btn.grid(row=3, column=0)
+btn2 = Button(janela, text="Clique", bg="green", command=desativar_entry)
+btn2.grid(row=4, column=0)
 
 janela.mainloop()
