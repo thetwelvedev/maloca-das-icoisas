55ce01059d00d9cdd2a950f9845c223700b175f0
Leonardo Castro <twelve.programming@gmail.com>
2024-09-26 16:38:32 -0400

    Aprendendo a usar o Entry

--- Módulo 3/MIC 010/00_estudo_tkinter/aula5.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula5.py
@@ -2,5 +2,12 @@ from tkinter import *
 
 janela = Tk()
 
+janela.title('Entry')
+janela.geometry("300x300")
+
+label = Label(janela, text="Entry") 
+label.grid(row=0, column=0) 
+entrada = Entry(janela, width=10) #adiciona o entrada de dados
+entrada.grid(row=0, column=1) 
 
 janela.mainloop()
