4ec10222314cbe5c3ca4bd076153af15f8992451
Leonardo Castro <twelve.programming@gmail.com>
2024-09-29 15:31:46 -0400

    Aula sobre Checkbutton em andamento

--- Módulo 3/MIC 010/00_estudo_tkinter/aula7.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula7.py
@@ -1,7 +1,12 @@
 from tkinter import *
+from tkinter.ttk import *
 
 janela = Tk()
-janela.title("")
-janela.geometry("500x500")
+janela.title("Checkbutton")
+janela.geometry("200x200")
+
+chek = Checkbutton(janela, text="escolha")
+chek.grid(row=0, column=0)
+#https://youtu.be/Rt2KsysJpGE?si=dD8Zr3R0uRwuOCNk
 
 janela.mainloop()
