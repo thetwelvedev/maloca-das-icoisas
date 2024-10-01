1fc6c7e919ae2ad37cdb7f95a4fb388b9d8ab745
Leonardo Castro <twelve.programming@gmail.com>
2024-10-01 14:47:24 -0400

    No final foi ensinado o uso do set() usando o parâmetros onvalue e offvalue

--- Módulo 3/MIC 010/00_estudo_tkinter/aula7.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula7.py
@@ -13,5 +13,6 @@ estado = BooleanVar()
 chek = Checkbutton(janela, text="escolha", var=estado, onvalue=1, offvalue=0 , command=teste)# o var recebe uma variavel, onde se tiver ativa recebe o valor da onvalue e se tiver desativada recebe o valor da offvalue
 chek.grid(row=0, column=0)
 
+estado.set(1)#esse aqui vai deixar o checkbutton ativado ou desativado de acordo com os valores do onvalue e offvalue
 
 janela.mainloop()
