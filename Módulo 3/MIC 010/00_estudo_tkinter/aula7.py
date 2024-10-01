98a91694c54635e2212af49475bfe0d9d1669deb
Leonardo Castro <twelve.programming@gmail.com>
2024-10-01 14:42:10 -0400

    Usando os parâmetros var, onvalue e offvalue do Checkbutton com valores inteiros

--- Módulo 3/MIC 010/00_estudo_tkinter/aula7.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula7.py
@@ -5,8 +5,13 @@ janela = Tk()
 janela.title("Checkbutton")
 janela.geometry("200x200")
 
-chek = Checkbutton(janela, text="escolha")
+def teste():
+    print(f"valor do checkbutton: {estado.get()}")
+
+estado = IntVar()
+
+chek = Checkbutton(janela, text="escolha", var=estado, onvalue=1, offvalue=0, command=teste)# o var recebe uma variavel, onde se tiver ativa recebe o valor da onvalue e se tiver desativada recebe o valor da offvalue
 chek.grid(row=0, column=0)
-#https://youtu.be/Rt2KsysJpGE?si=dD8Zr3R0uRwuOCNk
+
 
 janela.mainloop()
