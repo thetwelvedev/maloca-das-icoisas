d1a2952e518c3f66896c71bd803271a021f38fc7
Leonardo Castro <twelve.programming@gmail.com>
2024-10-01 14:44:40 -0400

    Usando os parâmetros var, onvalue e offvalue do Checkbutton com booleanos

--- Módulo 3/MIC 010/00_estudo_tkinter/aula7.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula7.py
@@ -8,9 +8,9 @@ janela.geometry("200x200")
 def teste():
     print(f"valor do checkbutton: {estado.get()}")
 
-estado = StringVar()
+estado = BooleanVar()
 
-chek = Checkbutton(janela, text="escolha", var=estado, onvalue="Ativado", offvalue="Desativado", command=teste)# o var recebe uma variavel, onde se tiver ativa recebe o valor da onvalue e se tiver desativada recebe o valor da offvalue
+chek = Checkbutton(janela, text="escolha", var=estado, onvalue=1, offvalue=0 , command=teste)# o var recebe uma variavel, onde se tiver ativa recebe o valor da onvalue e se tiver desativada recebe o valor da offvalue
 chek.grid(row=0, column=0)
 
 
