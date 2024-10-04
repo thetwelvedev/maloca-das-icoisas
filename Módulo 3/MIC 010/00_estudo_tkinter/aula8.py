f05702b45801b127fc793e43d09ad72dc36dffed
Leonardo Castro <twelve.programming@gmail.com>
2024-10-03 21:26:12 -0400

    Teste da função obter com booleanos

--- Módulo 3/MIC 010/00_estudo_tkinter/aula8.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula8.py
@@ -8,13 +8,13 @@ janela.geometry("200x200")
 def obter():
     print(f"O valor do botão selecionado é {selecionado.get()}.")
 
-selecionado = StringVar()
+selecionado = BooleanVar()
 
-rad1 = Radiobutton(janela, text="Primeiro", value="Primeiro", var=selecionado, command=obter)#Cada botão tem qu eter um valor diferente
+rad1 = Radiobutton(janela, text="Primeiro", value=0, var=selecionado, command=obter)#Cada botão tem que ter um valor diferente, pois se tiver o mesmo valor quando um for ativado o outro de mesmo valor vai ser também
 rad1.grid(row=0, column=0)
-rad2 = Radiobutton(janela, text="Segundo", value="Segundo", var=selecionado, command=obter)
+rad2 = Radiobutton(janela, text="Segundo", value=1, var=selecionado, command=obter)
 rad2.grid(row=0, column=1)
-rad3 = Radiobutton(janela, text="Terceiro", value="Terceiro", var=selecionado, command=obter)
+rad3 = Radiobutton(janela, text="Terceiro", value=0, var=selecionado, command=obter)
 rad3.grid(row=0, column=2)
 
 janela.mainloop()
