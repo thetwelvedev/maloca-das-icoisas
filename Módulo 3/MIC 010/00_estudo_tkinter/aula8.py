2f0a316562b7d3233b60d8b9a0d74dc6da417058
Leonardo Castro <twelve.programming@gmail.com>
2024-10-03 21:19:09 -0400

    Aprendendo o uso do Radiobutton

--- Módulo 3/MIC 010/00_estudo_tkinter/aula8.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula8.py
@@ -2,8 +2,14 @@ from tkinter import *
 from tkinter.ttk import *
 
 janela = Tk()
-janela.title("")
+janela.title("RadioButton")
 janela.geometry("200x200")
 
+rad1 = Radiobutton(janela, text="Primeiro", value=1)#Cada botão tem qu eter um valor diferente
+rad1.grid(row=0, column=0)
+rad2 = Radiobutton(janela, text="Segundo", value=2)
+rad2.grid(row=0, column=1)
+rad3 = Radiobutton(janela, text="Terceiro", value=3)
+rad3.grid(row=0, column=2)
 
 janela.mainloop()
