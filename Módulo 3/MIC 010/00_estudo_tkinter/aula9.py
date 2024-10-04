3c59e1aac16fc2788165a081252da69c24018aba
Leonardo Castro <twelve.programming@gmail.com>
2024-10-04 16:12:08 -0400

    n

--- Módulo 3/MIC 010/00_estudo_tkinter/aula9.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula9.py
@@ -8,9 +8,9 @@ janela.geometry("250x200")
 listbox = Listbox(janela, height=8)
 listbox.grid(row=0, column=0)
 
-listbox.insert(0, "PHP") #<index>, <item>
-listbox.insert(1, "Python")
-listbox.insert(2, "Java")
-listbox.insert(END, "JavaScript") # insere no final da lista
+linguagens_programacao = ["Python", "Java", "C", "C++", "JavaScript", "Ruby", "Go", "Swift"]
+
+for i in linguagens_programacao:
+    listbox.insert(END, i)
 
 janela.mainloop()
