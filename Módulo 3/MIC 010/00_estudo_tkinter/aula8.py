93aab76cc60e5ffcb40f1b49b80c23be18a6ac8e
Leonardo Castro <twelve.programming@gmail.com>
2024-10-03 21:22:40 -0400

    Teste da função obter com valores inteiros

--- Módulo 3/MIC 010/00_estudo_tkinter/aula8.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula8.py
@@ -5,11 +5,16 @@ janela = Tk()
 janela.title("RadioButton")
 janela.geometry("200x200")
 
-rad1 = Radiobutton(janela, text="Primeiro", value=1)#Cada botão tem qu eter um valor diferente
+def obter():
+    print(f"O valor do botão selecionado é {selecionado.get()}.")
+
+selecionado = IntVar()
+
+rad1 = Radiobutton(janela, text="Primeiro", value=1, var=selecionado, command=obter)#Cada botão tem qu eter um valor diferente
 rad1.grid(row=0, column=0)
-rad2 = Radiobutton(janela, text="Segundo", value=2)
+rad2 = Radiobutton(janela, text="Segundo", value=2, var=selecionado, command=obter)
 rad2.grid(row=0, column=1)
-rad3 = Radiobutton(janela, text="Terceiro", value=3)
+rad3 = Radiobutton(janela, text="Terceiro", value=3, var=selecionado, command=obter)
 rad3.grid(row=0, column=2)
 
 janela.mainloop()
