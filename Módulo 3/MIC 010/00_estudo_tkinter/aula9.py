e9d266cec66bd5f782b2211203d938d46df6f2f9
Leonardo Castro <twelve.programming@gmail.com>
2024-10-04 16:08:00 -0400

    Aprendendo uso do insert para inserir elementos dentro da listbox

--- Módulo 3/MIC 010/00_estudo_tkinter/aula9.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula9.py
@@ -8,4 +8,8 @@ janela.geometry("250x200")
 listbox = Listbox(janela, height=8)
 listbox.grid(row=0, column=0)
 
+listbox.insert(0, "PHP") #<index>, <item>
+listbox.insert(1, "Python")
+listbox.insert(2, "Java")
+
 janela.mainloop()
