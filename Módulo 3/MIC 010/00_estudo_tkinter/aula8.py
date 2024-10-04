630e42deb03b0b6e968d0509a2419c7a2cd29014
Leonardo Castro <twelve.programming@gmail.com>
2024-10-03 21:23:42 -0400

    Teste da função obter com strings

--- Módulo 3/MIC 010/00_estudo_tkinter/aula8.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula8.py
@@ -8,13 +8,13 @@ janela.geometry("200x200")
 def obter():
     print(f"O valor do botão selecionado é {selecionado.get()}.")
 
-selecionado = IntVar()
+selecionado = StringVar()
 
-rad1 = Radiobutton(janela, text="Primeiro", value=1, var=selecionado, command=obter)#Cada botão tem qu eter um valor diferente
+rad1 = Radiobutton(janela, text="Primeiro", value="Primeiro", var=selecionado, command=obter)#Cada botão tem qu eter um valor diferente
 rad1.grid(row=0, column=0)
-rad2 = Radiobutton(janela, text="Segundo", value=2, var=selecionado, command=obter)
+rad2 = Radiobutton(janela, text="Segundo", value="Segundo", var=selecionado, command=obter)
 rad2.grid(row=0, column=1)
-rad3 = Radiobutton(janela, text="Terceiro", value=3, var=selecionado, command=obter)
+rad3 = Radiobutton(janela, text="Terceiro", value="Terceiro", var=selecionado, command=obter)
 rad3.grid(row=0, column=2)
 
 janela.mainloop()
