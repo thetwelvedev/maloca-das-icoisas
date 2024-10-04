825d516e6114c1d8884ce1444d650b7a6dc226c7
Leonardo Castro <twelve.programming@gmail.com>
2024-10-04 16:40:50 -0400

    Aprendendo situções com o listbox

--- Módulo 3/MIC 010/00_estudo_tkinter/aula9.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula9.py
@@ -8,16 +8,18 @@ janela.geometry("250x200")
 listbox = Listbox(janela, height=8)
 listbox.grid(row=0, column=0)
 
+def test():
+    print(listbox.get(ACTIVE))
+    l = Label(janela, text=listbox.get(ACTIVE), width=20)
+    l.grid(row=0, column=1)
+
+
 linguagens_programacao = ["Python", "Java", "C", "C++", "JavaScript", "Ruby", "Go", "Swift"]
 
 for i in linguagens_programacao:
     listbox.insert(END, i)
 
-#obtem o primeiro item da lista
-print(listbox.get(ACTIVE)) #Usar print para visualizar
-#obtem item dO INDEX
-print(listbox.get(1))
-#obtem o INTERVALO
-print(listbox.get(1, 2))
+b = Button(janela, text="Imprimir", command=test)
+b.grid(row=1, column=0)
 
 janela.mainloop()
