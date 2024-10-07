9254b38a6737e046b3eb37c6b679fc207fb476ee
Leonardo Castro <twelve.programming@gmail.com>
2024-10-07 00:36:12 -0400

    Aprendendo a como usar o parâmetro columnspan

--- Módulo 3/MIC 010/00_estudo_tkinter/aula10.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula10.py
@@ -4,11 +4,11 @@ janela = Tk()
 janela.title("Frame")
 janela.geometry("300x300")
 
-frame_1 = Frame(janela, width=300, height=100, bg="red")
+frame_1 = Frame(janela, width=150, height=150, bg="red")
 frame_1.grid(row=0, column=0)
-frame_2 = Frame(janela, width=300, height=100, bg="green")
-frame_2.grid(row=1, column=0)
-frame_3 = Frame(janela, width=300, height=100, bg="blue")
-frame_3.grid(row=2, column=0)
+frame_2 = Frame(janela, width=150, height=150, bg="green")
+frame_2.grid(row=0, column=1)
+frame_3 = Frame(janela, width=300, height=150, bg="blue")
+frame_3.grid(row=1, column=0, columnspan=2) # o columnspan defne o número de de coluna que o frame vai preencher
 
 janela.mainloop()
