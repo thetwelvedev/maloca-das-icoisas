b8e6adf7e9cf71a6acf53e914cfe82a290eab3a4
Leonardo Castro <twelve.programming@gmail.com>
2024-10-07 00:30:38 -0400

    Aprendendo o uso do frame

--- Módulo 3/MIC 010/00_estudo_tkinter/aula10.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula10.py
@@ -1,9 +1,14 @@
 from tkinter import *
-from tkinter.ttk import *
 
 janela = Tk()
 janela.title("Frame")
-janela.geometry("250x200")
+janela.geometry("300x300")
 
+frame_1 = Frame(janela, width=300, height=100, bg="red")
+frame_1.grid(row=0, column=0)
+frame_2 = Frame(janela, width=300, height=100, bg="green")
+frame_2.grid(row=1, column=0)
+frame_3 = Frame(janela, width=300, height=100, bg="blue")
+frame_3.grid(row=2, column=0)
 
 janela.mainloop()
