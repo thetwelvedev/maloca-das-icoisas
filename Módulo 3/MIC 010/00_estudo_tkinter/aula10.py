077b806bf8f535e7d4777340ed26ec76a9f84a04
Leonardo Castro <twelve.programming@gmail.com>
2024-10-07 00:46:14 -0400

    Como colocar algo dentro do frame

--- Módulo 3/MIC 010/00_estudo_tkinter/aula10.py
+++ Módulo 3/MIC 010/00_estudo_tkinter/aula10.py
@@ -4,11 +4,15 @@ janela = Tk()
 janela.title("Frame")
 janela.geometry("300x300")
 
-frame_1 = Frame(janela, width=150, height=150, bg="red")
+frame_1 = Frame(janela, width=150, height=200, bg="red")
 frame_1.grid(row=0, column=0)
-frame_2 = Frame(janela, width=150, height=150, bg="green")
+label = Label(frame_1, width=20, text="1", fg="black")
+label.grid(row=0, column=0) 
+
+frame_2 = Frame(janela, width=150, height=200, bg="green")
 frame_2.grid(row=0, column=1)
-frame_3 = Frame(janela, width=300, height=150, bg="blue")
+ 
+frame_3 = Frame(janela, width=300, height=200, bg="blue")
 frame_3.grid(row=1, column=0, columnspan=2) # o columnspan defne o número de de coluna que o frame vai preencher
 
 janela.mainloop()
